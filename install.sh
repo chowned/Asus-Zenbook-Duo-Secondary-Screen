79b9bddb566e43c66f4581ff3ef15b93578050ce
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2023-03-14 12:53:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo modprobe acpi_call
 # required for gui
 sudo pacman -S gtk3 
 
-gcc `pkg-config --cflags gtk+-3.0` -o asus_screen_brightness_controller gui_to_run_script.c `pkg-config --libs gtk+-3.0
+gcc `pkg-config --cflags gtk+-3.0` -o asus_screen_brightness_controller gui_to_run_script.c `pkg-config --libs gtk+-3.0`
 
 #gcc `pkg-config --cflags gtk+-3.0` -o asus_screen_brightness_controller gui_to_run_script.c `pkg-config --libs gtk+-3.0`
 
